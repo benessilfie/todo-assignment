73aaf3404b86880e68bb056d6d33a432af8fe3c1
benessilfie <bquansah007@gmail.com>
2022-10-27 17:37:38 +0000

    create registration views

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,16 @@
+class AuthController < ApplicationController
+    
+    def sign_in
+    end
+    
+    def sign_up
+    end
+
+    def login 
+    end
+
+    def create_user
+    end
+    
+end
+    
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
   root "pages#home"
-
-  
-
   resources :todos
+
+  scope :auth do
+    get "sign-in", to: "auth#sign_in", as: :sign_in
+    post "sign-in", to: "auth#login", as: :login
+    get "sign-up", to: "auth#sign_up", as: :sign_up
+    post "sign-up", to: "auth#create_user", as: :create_user
+  end
+
 end
