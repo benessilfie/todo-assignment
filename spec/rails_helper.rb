8ef7e445d8d9f18e43fa9a6d7248f9f2d6c474cf
benessilfie <bquansah007@gmail.com>
2023-01-06 20:57:34 +0000

    remove DatabaseCleaner configuration for RSpec tests from rails_helper.rb file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,24 +63,4 @@ RSpec.configure do |config|
 
   config.include FactoryBot::Syntax::Methods
 end
-
-
-  RSpec.configure do |config|
-  
-    config.before(:suite) do
-      DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
-    end
-  
-    config.before(:each) do
-      DatabaseCleaner.strategy = :transaction
-    end
-  
-    config.before(:each) do
-      DatabaseCleaner.start
-    end
-  
-    config.after(:each) do
-      DatabaseCleaner.clean
-    end
-  
-  end
+ 
